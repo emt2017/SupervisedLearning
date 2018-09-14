d100b6a2726aec7c6c4b30d6664a16d77e19f3b7
emt2017 <emt9q7@mail.umkc.edu>
2018-09-14 04:08:53 -0500

    Add files via upload

---
+++ Mini_project#1 (1)/Question1.m
@@ -0,0 +1,43 @@
+clc
+clear all
+close all 
+
+%load data
+load Mini_project#1/featureMat_Latex_test_bioLBP.mat
+load Mini_project#1/featureMat_Latex_train_bioLBP.mat
+load Mini_project#1/featureMat_liv_test_bioLBP.mat
+load Mini_project#1/featureMat_liv_train_bioLBP.mat
+
+%train data
+train_live_features = featureMat_liv_train_bioLBP;
+train_fake_features = featureMat_Latex_train_bioLBP;
+
+%concatenate features to create training data
+train = [train_live_features' train_fake_features'];
+
+%define labels 1000(1's for live) 200(0's for fakes)
+labels = cat(2, ones(1,1000), zeros(1,200));
+
+%Train Naive Bayes classifier --- latex
+model = fitcnb(train', labels');
+model.Prior
+%-------------------TEST--------------------------------------
+%test data
+test_live_features = featureMat_liv_test_bioLBP;
+test_fake_features = featureMat_Latex_test_bioLBP;
+
+test = [test_live_features' test_fake_features'];
+
+
+%Compute loss and Resubstitution error
+%Calculate Loss 
+loss = loss(model,test',labels') %Test the classifier on test set? 
+
+%Resubstitution Error
+resubError = resubLoss(model)
+
+
+
+
+
+
